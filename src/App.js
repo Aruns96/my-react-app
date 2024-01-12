c078fb7b03398fdf24f233bc234699054a4a98ea
Aruns96 <arun509577@gmail.com>
2024-01-12 15:45:43 +0530

    using js in jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import ExpenseItem from "./components/ExpenseItem";
-import ExpenseItem1 from "./components/ExpenseItem1";
+
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem></ExpenseItem>
-      <ExpenseItem1></ExpenseItem1>
+     
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,22 @@
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    return <h2>Expense Items</h2>
+function ExpenseItem() {
+    
+    const eName="Petrol";
+    const eDate = new Date(2024,1,12);
+    const eAmout = 100;
+    const eLocation = "kollam"
+
+  return (
+    <div className="expense-item">
+      <div>{eDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{eName}</h2>
+        <h3>{eLocation}</h3>
+        <div className="expense-item__price">{eAmout}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
