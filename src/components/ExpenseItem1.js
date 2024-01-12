8b4e6d0fc41b183dc79451890317842b25347c61
Aruns96 <arun509577@gmail.com>
2024-01-12 12:50:11 +0530

    added expense item

---
+++ src/components/ExpenseItem1.js
@@ -0,0 +1,11 @@
+function ExpenseItem1() {
+  return (
+    <div>
+     <div>food 100</div> 
+     <div>movie 200</div>
+     <div>petrol 300</div> 
+    </div>
+  );
+}
+
+export default ExpenseItem1;
