ac393f2c562dfccfa8a5ecedf591447c9106396b
Aruns96 <arun509577@gmail.com>
2024-01-15 16:53:32 +0530

    react states

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,11 +1,22 @@
 import './ExpenseItem.css'
-import React from 'react';
+import React ,{ useState }from 'react';
 const ExpenseDetails = (props) => {
+  const [title,setTitle] = useState(props.title)
+  const [addAmount,setAddAmout] = useState(props.amount)
+  const clickHandler = () => {
+    setTitle("new")
+    console.log(title)
+  }
+  const addHanler = () => {
+   setAddAmout("$100")
+  }
   return (
     <div className="expense-item__description">
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <h3>{props.location}</h3>
-      <div className="expense-item__price">{props.amount}</div>
+      <div className="expense-item__price">{addAmount}</div>
+      <button onClick={clickHandler}>click</button>
+      <button onClick={addHanler}>add</button>
     </div>
   );
 }
